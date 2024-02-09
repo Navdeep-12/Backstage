4c236a57595ff80146710cd46f41466a30078e57
Navdeep Singh <navdeep.singh@ivedha.com>
2024-02-09 10:02:59 -0500

    removing cron to disable notifications

--- packages/backend/src/plugins/permission.ts
+++ packages/backend/src/plugins/permission.ts
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import { BackstageIdentityResponse } from '@backstage/plugin-auth-node';
 import { createRouter } from '@backstage/plugin-permission-backend';
@@ -18,29 +32,31 @@ import {
 } from '@backstage/plugin-playlist-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import { catalogConditions, createCatalogConditionalDecision } from '@backstage/plugin-catalog-backend/alpha';
-import { catalogEntityCreatePermission, catalogEntityDeletePermission, catalogEntityReadPermission, catalogPermissions } from '@backstage/plugin-catalog-common/alpha';
+import {
+  catalogConditions,
+  createCatalogConditionalDecision,
+} from '@backstage/plugin-catalog-backend/alpha';
+import {
+  catalogEntityCreatePermission,
+  catalogEntityDeletePermission,
+  catalogEntityReadPermission,
+  catalogPermissions,
+} from '@backstage/plugin-catalog-common';
 
 class ExamplePermissionPolicy implements PermissionPolicy {
-
   async handle(
     request: PolicyQuery,
     user?: BackstageIdentityResponse,
   ): Promise<PolicyDecision> {
-
     // LOGIC if any non registed user tris to login into the backsage application
     // He/she would not be able to create or read anything in the application
 
-    if (user === undefined) {
-      const visibility = request.permission.name === 'catalog.entity.read'
-      const creating = request.permission.name === 'catalog.entity.create'
-      if (visibility || creating) {
-        return { result: AuthorizeResult.DENY }
-      }
-    }
-
-    if (user && Object.keys(user).length && user.identity.ownershipEntityRefs.length) {
-      const permission = request.permission as any
+    if (
+      user &&
+      Object.keys(user).length &&
+      user.identity.ownershipEntityRefs.length
+    ) {
+      const permission = request.permission as any;
       if (permission.resourceType === 'catalog-entity') {
         if (isPermission(request.permission, catalogEntityDeletePermission)) {
           return createCatalogConditionalDecision(
@@ -51,16 +67,16 @@ class ExamplePermissionPolicy implements PermissionPolicy {
           );
         }
 
-    //  console.log("user -------" , user.identity.ownershipEntityRefs)
-    //  if (isPermission(request.permission, catalogEntityReadPermission)) {
-    //    console.log("inside the read function / method ----------")
-    //    return createCatalogConditionalDecision(
-    //      request.permission,
-    //      catalogConditions.isEntityOwner({
-    //        claims: user?.identity.ownershipEntityRefs ?? [],
-    //      }),
-    //    );
-    //  }
+        //  console.log("user -------" , user.identity.ownershipEntityRefs)
+        //  if (isPermission(request.permission, catalogEntityReadPermission)) {
+        //    console.log("inside the read function / method ----------")
+        //    return createCatalogConditionalDecision(
+        //      request.permission,
+        //      catalogConditions.isEntityOwner({
+        //        claims: user?.identity.ownershipEntityRefs ?? [],
+        //      }),
+        //    );
+        //  }
       }
     }
 
